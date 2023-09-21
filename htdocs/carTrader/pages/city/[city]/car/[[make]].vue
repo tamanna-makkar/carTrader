2bcbcfabdcea00303e05de06f4c3816bc1308294
tamanna <tamanna@smartshore.nl>
2023-09-21 18:00:19 +0530

    NUXT3 app isin-progress

---
+++ htdocs/carTrader/pages/city/[city]/car/[[make]].vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="mx-auto mt-4 max-w-7xl px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+  >
+    <div class="mt-32 flex">
+      <CarSideBar />
+      <CarCard />
+    </div>
+  </div>
+</template>
---
+++ htdocs/carTrader/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <CarHero />
+  </div>
+</template>
